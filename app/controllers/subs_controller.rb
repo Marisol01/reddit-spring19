c99a6eb9637572f744e92aa893e9a24bb61c66a8
Marisoln Araujo <u167272@gmail.com>
2019-02-27 19:16:09 -0700

    changes

---
+++ app/controllers/subs_controller.rb
@@ -0,0 +1,20 @@
+class SubsController < ApplicationController
+  # views user in teraction logic at minimum
+  #controller / routes directing traffic, actions
+  #models all logic on the table /record
+
+  # skinny controllers / fat models
+
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'subs/index'
+  get 'subs/show'
+  get 'subs/new'
+  get 'subs/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'subs#index'
+  resources :subs do
+    resources :topics
+  end
 end
